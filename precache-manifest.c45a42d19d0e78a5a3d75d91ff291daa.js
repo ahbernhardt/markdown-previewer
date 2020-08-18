0ff000c6ca2f9cebe01be60066948fa448132188
anguyen0208 <a.nguyen0208@gmail.com>
2020-08-18 17:02:43 -0500

    Updates

---
+++ precache-manifest.c45a42d19d0e78a5a3d75d91ff291daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f897f7d624348394d951536ffcfab529",
+    "url": "/fcc-markdown-previewer/index.html"
+  },
+  {
+    "revision": "66224b980b21847c22d8",
+    "url": "/fcc-markdown-previewer/static/css/main.0070ca5e.chunk.css"
+  },
+  {
+    "revision": "340b079d057e25fc2c08",
+    "url": "/fcc-markdown-previewer/static/js/2.10d7d4a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-markdown-previewer/static/js/2.10d7d4a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66224b980b21847c22d8",
+    "url": "/fcc-markdown-previewer/static/js/main.0783effd.chunk.js"
+  },
+  {
+    "revision": "53b2abfe119e19b7090d",
+    "url": "/fcc-markdown-previewer/static/js/runtime-main.4cde48bf.js"
+  }
+]);
